ccb41fb8f4308c4b4aac5b984ccd55c7e143d6f1
zxcbvnmditto <chccheng@ucdavis.edu>
2021-04-13 18:39:07 -0700

    first commit

---
+++ app/src/main/java/com/example/usc_film/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.usc_film;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, new HomeFragment()).commit(); // Start Home when app first launches
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.home_btn:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.search_btn:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.watchlist_btn:
+                    selectedFragment = new WatchlistFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
+}
