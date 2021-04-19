762c064b8fe3babec289d6ce47e4a704cf016ee7
zxcbvnmditto <chccheng@ucdavis.edu>
2021-04-18 22:50:39 -0700

    Add Searchview

--- app/src/main/java/com/example/usc_film/search/SearchFragment.java
+++ app/src/main/java/com/example/usc_film/search/SearchFragment.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.example.usc_film.R;
@@ -16,6 +18,11 @@ public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        SearchView searchView = view.findViewById(R.id.search_bar);
+//        searchView.fo
+//        searchView.setQuery("Search Movies and Tvs", false);
+
+        return view;
     }
 }
